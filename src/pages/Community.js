66f81504ffcd3165e97f4cfa4f3b20cadf12d83f
YANGDAYOUNG <jc1525@hanmail.net>
2021-05-10 14:13:51 +0900

    게시판 목록까지는 고침

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PostMain from './post/PostMain';
 
 const Community = () => {
     return(
         <div>
             <h2>Community Page</h2>
+
+            <div className="App">
+                <PostMain />
+            </div>
         </div>
     )
 }
