b9d83f93ab1c922196df2c72a9aa0a11619b45d8
YANGDAYOUNG <jc1525@hanmail.net>
2021-05-16 21:14:02 +0900

    added map and marker

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,12 @@ const Header = () => {
     return (
         
         <header className="header">
-            
+                          
+            <img src={mypic} alt='mypic' id="homebutton" />
                
-                    <img src={mypic} alt='mypic' id="homebutton" />
-               
-                <h1>STCAT HOUSE</h1>
-                
-            
-                
-           
-           
-            
-                
+            <h1>STCAT HOUSE</h1>
+                                       
+                                      
         </header>
        
         
